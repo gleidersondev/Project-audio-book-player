57232a72e8941072c5fd0ddcaea83053c936c6b3
gleidersondev <gleidersondev@gmail.com>
2024-01-30 00:31:48 -0300

    Configurado botão de reprodução e pause

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-const audioAnterior = document.getElementById('anterior');
-const ouvirAudio = document.getElementById('play-pause');
-const proximoAudio = document.getElementById('proximo');
+const audio = document.getElementById('audio-capitulo');
+const btnAnterior = document.getElementById('anterior');
+const btnOuvirAudio = document.getElementById('play-pause');
+const btnProximo = document.getElementById('proximo');
 
 let emReproducao = false;
 
+console.log(btnOuvirAudio);
+
+const reproducaoDeAudio = () => {
+  if (!emReproducao) {
+    emReproducao = true;
+    audio.play();
+    btnOuvirAudio.classList.add('bi', 'bi-play-circle');
+  } else {
+    audio.pause();
+    btnOuvirAudio.classList.remove('bi', 'bi-play-circle');
+    emReproducao = false;
+    btnOuvirAudio.classList.add('bi', 'bi-pause-circle');
+  }
+};
+
+btnOuvirAudio.addEventListener("click", reproducaoDeAudio);
 
