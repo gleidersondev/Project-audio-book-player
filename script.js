0b2a53f547f29f8c9237d47829367ed10f8a32e7
gleidersondev <gleidersondev@gmail.com>
2024-01-29 23:25:45 -0300

    Insere ids e inicia logica para manipulação do DOM

---
+++ script.js
@@ -0,0 +1,7 @@
+const audioAnterior = document.getElementById('anterior');
+const ouvirAudio = document.getElementById('play-pause');
+const proximoAudio = document.getElementById('proximo');
+
+let emReproducao = false;
+
+
